e2976783dfce8b3feb236a08ee3c3aaf6affcc98
Michal Kowol <michal.kowol@vimn.com>
2022-05-19 10:42:59 +0200

    Add EV / EBIT & EV / EBITDA

--- gpw.py
+++ gpw.py
@@ -19,6 +19,8 @@ files = {
     'QMax26wCh[id]': (None, 'QMax26wCh'),
     'QMin52wCh[id]': (None, 'QMin52wCh'),
     'QMax52wCh[id]': (None, 'QMax52wCh'),
+    'REVEBITCurrent[id]': (None, 'REVEBITCurrent'),
+    'REVEBITDACurrent[id]': (None, 'REVEBITDACurrent'),
 }
 
 response = requests.post('https://www.biznesradar.pl/skaner-akcji-get-json/', files=files)
@@ -26,7 +28,7 @@ json = response.json()
 
 
 with open('gpw.csv', 'w', newline='') as csvfile:
-    fieldnames = ['Ticker', 'Nazwa', 'Aktualny kurs', 'Rynek', 'Sektor', 'Branża', 'Raport', 'Cena / Wartość księgowa', 'Cena / Zysk', 'ROE', 'Płynność bieżąca', 'Kapitał własny akcjonariuszy jednostki dominującej [tys. PLN]', 'Altman EM-Score', 'Piotroski F-Score', 'Zmiana kursu 12m [%]', 'Zmiana kursu 24m [%]', 'Min 26 tyg. [%]', 'Max 26 tyg. [%]', 'Min 52 tyg. [%]', 'Max 52 tyg. [%]']
+    fieldnames = ['Ticker', 'Nazwa', 'Aktualny kurs', 'Rynek', 'Sektor', 'Branża', 'Raport', 'Cena / Wartość księgowa', 'Cena / Zysk', 'ROE', 'Płynność bieżąca', 'Kapitał własny akcjonariuszy jednostki dominującej [tys. PLN]', 'Altman EM-Score', 'Piotroski F-Score', 'Zmiana kursu 12m [%]', 'Zmiana kursu 24m [%]', 'Min 26 tyg. [%]', 'Max 26 tyg. [%]', 'Min 52 tyg. [%]', 'Max 52 tyg. [%]', 'EV / EBIT', 'EV / EBITDA']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
 
     writer.writeheader()
@@ -53,7 +55,9 @@ with open('gpw.csv', 'w', newline='') as csvfile:
             'Min 26 tyg. [%]': company.get('QMin26wCh', None),
             'Max 26 tyg. [%]': company.get('QMax26wCh', None),
             'Min 52 tyg. [%]': company.get('QMin52wCh', None),
-            'Max 52 tyg. [%]': company.get('QMax52wCh', None)
+            'Max 52 tyg. [%]': company.get('QMax52wCh', None),
+            'EV / EBIT': company.get('REVEBITCurrent', None),
+            'EV / EBITDA': company.get('REVEBITDACurrent', None),
         })
 
 
